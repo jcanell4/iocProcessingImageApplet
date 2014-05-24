daa2e900c8cfa2b16938262115a294650c1d6a54
Dani Criado <dani.criado.casas@gmail.com>
2014-05-24 08:01:57 +0200

    First commit Dani

---
+++ ioc/wiki/processingmanager/excepcions/ProcessingLoaderException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ioc.wiki.processingmanager.excepcions;
+
+/**
+ *
+ * @author Daniel Criado Casas<dani.criado.casas@gmail.com>
+ */
+public class ProcessingLoaderException extends ProcessingException{
+    
+    public ProcessingLoaderException(Exception ex) {
+        super(ex);
+    }
+    
+}
---
+++ ioc/wiki/processingmanager/excepcions/ProcessingRtURLException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ioc.wiki.processingmanager.excepcions;
+
+/**
+ *
+ * @author Daniel Criado Casas<dani.criado.casas@gmail.com>
+ */
+public class ProcessingRtURLException extends ProcessingRtException{
+
+    public ProcessingRtURLException(Exception ex) {
+        super(ex);
+    }
+    
+    
+}
